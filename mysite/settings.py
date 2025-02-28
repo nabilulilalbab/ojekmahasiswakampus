4c289e5dac66bc1ba5db5658667854e4b5b5599c
nabilulilalbab <nabilulilalbab78@gmail.com>
2025-02-28 22:39:36 +0700

    add distance count & range price

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ojekmahasiswakampus.biz.id","ojekmahasiswakampus.sgp.dom.my.id","localhost", "127.0.0.1" , "ojekmahasiswakampus.biz.id","*"]
 
